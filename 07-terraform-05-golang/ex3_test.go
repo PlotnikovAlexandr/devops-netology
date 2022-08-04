8a1210231c217a8d984ba4407269a791f31ddd5f
AlexanderPlotnikov <sir.carpenter@yandex.ru>
2022-08-04 14:52:04 +0300

    add tests

--- 07-terraform-05-golang/ex3_test.go
+++ 07-terraform-05-golang/ex3_test.go
@@ -6,7 +6,14 @@ func Test_main(t *testing.T) {
 	tests := []struct {
 		name string
 	}{
-		// TODO: Add test cases.
+		for i := 1; i <= 1000; i++ {
+			if ((i - 1) % 10) == 0 {
+				fmt.Print()
+			}
+			if (i % 3) == 0 {
+				fmt.Print(i, ", ")
+			}
+		}
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
