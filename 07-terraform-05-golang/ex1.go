ce89411ad30772abc824ce86a41fd89da8c79df2
AlexanderPlotnikov <sir.carpenter@yandex.ru>
2022-08-04 14:58:46 +0300

    homework

--- 07-terraform-05-golang/ex1.go
+++ 07-terraform-05-golang/ex1.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Print("Vvedite chislo: ")
+	fmt.Print("Введите число: ")
 	var input float64
 
 	fmt.Scanf("%f", &input)
 	result := input * float64(0.3048)
 	resultm := math.Round(result)
 	resultf := fmt.Sprintf("%.2f", result)
-	fmt.Println("Chislo v metrah:", resultm)
-	fmt.Println("Chislo v metrah s tochkoi:", resultf)
+	fmt.Println("Число в метрах:", resultm)
+	fmt.Println("Число в метрах с точностью:", resultf)
 }
